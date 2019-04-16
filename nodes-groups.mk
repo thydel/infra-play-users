ae77686a671e0b993bc2330dc59ae36aa4ac830b
Thierry Delamare <t.delamare@laposte.net>
2019-04-16 13:41:48 +0200

    Adds cross-domain and epiconcept.fr inventory

--- nodes-groups.mk
+++ nodes-groups.mk
@@ -4,8 +4,12 @@ MAKEFLAGS += -Rr
 
 top:; @date
 
-self := inventory/nodes-groups.INI
-$(self): ext/nodes-groups/out/static_groups.ini; install $< $@
+nodes-groups := inventory/nodes-groups.INI
+$(nodes-groups): ext/nodes-groups/out/static_groups.ini; install $< $@
 
-main: $(self)
+cross-domain := inventory/cross-domain.INI
+epiconcept.fr := inventory/epiconcept.fr.INI
+$(cross-domain) $(epiconcept.fr): inventory/% : ext/nodes-groups/%; install $< $@
+
+main: $(nodes-groups) $(cross-domain) $(epiconcept.fr)
 .PHONY: main
