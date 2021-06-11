711c4d89dae53870e743970343f53df1d26aba0a
Thierry Delamare <t.delamare@laposte.net>
2021-06-11 16:52:27 +0200

    Allow refresh nodes data via stone

--- alien-group.mk
+++ alien-group.mk
@@ -21,15 +21,19 @@ tmp := tmp
 group.d := $(tmp)/group
 $(group.d):; mkdir -p $@
 
-$(tmp)/jc: | $(group.d); git -C $(@D) clone git@github.com:kellyjonbrazil/jc.git
+stone := $(tmp)/.stone
+$(stone): | $(tmp); touch $@
+stone: phony | $(tmp); touch $(stone)
+
+$(tmp)/jc: | $(group.d); test -d $@ || git -C $(@D) clone git@github.com:kellyjonbrazil/jc.git
 jc := /usr/local/bin/jc
 ~ := $(jc)
-$~: | $(tmp)/jc; (cd $<; pip3 install --no-color --upgrade --user -e .)
+$~: | $(tmp)/jc; (cd $|; pip3 install --no-color --upgrade --user -e .)
 jc: phony $~
 
 ~ := $(group.d)/%
 $~: jq := map(select(.group_name == ("sudo", "ssh")) | {(.group_name): .members}) | add | { ($$node): . }
-$~: $(jc) | $(group.d); ssh $(@F) cat /etc/group | jc --group | jq --arg node $(@F) '$(jq)' > $@
+$~: $(jc) $(stone) | $(group.d); ssh $(@F) cat /etc/group | jc --group | jq --arg node $(@F) '$(jq)' > $@
 
 alien.nodes := esistn2 esistn3
 alien.nodes.fqnd := $(alien.nodes:%=$(group.d)/%.esisdmz)
@@ -45,10 +49,6 @@ nodes: phony $~
 
 alien-groups := repo/infra-data-users/alien-groups.js
 
-stone := $(tmp)/.stone
-$(stone): | $(tmp); touch $@
-stone: phony | $(tmp); touch $(stone)
-
 ~ := $(tmp)/all.json
 $~: data := $$users, $$nodes
 $~: jq := . as [ $(data) ] | { $(data) }
@@ -62,7 +62,7 @@ todo: phony $~
 - := $~
 
 ~ := $(tmp)/todo.sh
-$~: alien-group-shell.jq $-; jq -rf $^ > $@
+$~: alien-group-shell.jq $-; jq -rf $^ | install /dev/stdin $@
 sh: phony $~
 
 yq := /usr/local/bin/yq
